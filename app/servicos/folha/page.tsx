bdd932f6eb54097cd4f682b0a29933f4f498761a
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2026-04-05 00:23:29 -0300

    adicioando page no folha

---
+++ app/servicos/folha/page.tsx
@@ -0,0 +1,8 @@
+export default function Folha() {
+  return (
+    <div>
+      <h1>Folha de Pagamento</h1>
+      <p>Em breve, mais informações sobre nosso serviço de folha de pagamento.</p>
+    </div>
+  );
+}
